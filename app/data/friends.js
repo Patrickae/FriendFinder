af8463f774c83ee8f86b9ac4be60599fec7f5e78
Andy <Andy@Admins-MacBook-Air.local>
2017-05-18 22:34:58 -0400

    commit

--- app/data/friends.js
+++ app/data/friends.js
@@ -7,6 +7,22 @@ var friends = [{
 					name:"Bill",
 					image: "http://sites.austincc.edu/jrnl/wp-content/uploads/sites/50/2015/07/placeholder.gif",
 					answers:[5,5,5,5,5,5,5,5,5,5]
+				},
+				{
+					name: "Tony",
+					image:"https://www.colourbox.com/preview/6490319-caveman-happy-about-having-a-banana-to-eat.jpg",
+					answers: [3,4,1,5,3,1,2,4,3,2]
+
+				},
+				{
+					name:"Susan",
+					image:"http://www.ft.lk/ftadmin/wp-content/uploads/2012/05/Vegetarian-Picture-for-poya-release.jpg",
+					answers:[5,4,3,4,2,1,2,3,4,3]
+				},
+				{
+					name: "Gertrude",
+					image:"https://st2.depositphotos.com/2170303/5449/i/950/depositphotos_54498755-stock-photo-cool-grandma.jpg",
+					answers:[1,4,3,2,4,2,4,2,4,5]
 				}];
 
 module.exports = friends;
