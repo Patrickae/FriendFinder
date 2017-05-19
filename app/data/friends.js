a089fbfe27fb20b187cb3bbb4ba1999c72ed5bd1
Andy <Andy@Admins-MacBook-Air.local>
2017-05-18 20:49:11 -0400

    commit change

---
+++ app/data/friends.js
@@ -0,0 +1,12 @@
+var friends = [{
+					name: "Jim",
+					image: "https://placeholdit.imgix.net/~text?txtsize=28&bg=0099ff&txtclr=ffffff&txt=300%C3%97300&w=300&h=300",
+					answers: [1,1,1,1,1,1,1,1,1,1]
+				},
+				{
+					name:"Bill",
+					image: "http://sites.austincc.edu/jrnl/wp-content/uploads/sites/50/2015/07/placeholder.gif",
+					answers:[5,5,5,5,5,5,5,5,5,5]
+				}];
+
+module.exports = friends;
